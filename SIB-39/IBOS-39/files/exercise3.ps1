26b851869d6f06a79a9408dedf74b8677f5bf4b6
ivanlitovka <106375750+ivanlitovka@users.noreply.github.com>
2024-05-15 12:15:55 +0300

    Add files via upload

---
+++ SIB-39/IBOS-39/files/exercise3.ps1
@@ -0,0 +1,7 @@
+﻿if  ((Test-Path -Path $args[0] -PathType Container) -eq $true)
+{"This is direcory"}
+elseif ((Test-Path -Path $args[0] -PathType Leaf) -eq $true)
+{"This is file"}
+else
+{"Object not exist"}
+
---
+++ SIB-39/IBOS-39/files/exercise4.ps1
@@ -0,0 +1,6 @@
+﻿if  ($args[0] -eq "encrypt")
+{[Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($args[1]))}
+elseif ($args[0] -eq "decrypt")
+{[Text.Encoding]::Utf8.GetString([Convert]::FromBase64String($args[1]))}
+else
+{"Use next sintax: exercise4.ps1 encrypt|decrypt text"}
