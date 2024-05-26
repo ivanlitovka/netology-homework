ed951d6e134115bb5cba8b4684892d7153f19717
Ivan Litovka <ivan.litovka@gmail.com>
2024-05-26 22:39:56 +0300

    python

--- SIB-39/IBOS-39/files/nmap.py
+++ SIB-39/IBOS-39/files/nmap.py
@@ -1,6 +1,6 @@
 import nmap3
 
-
-scan = nmap3.NmapScanTechniques()
-result = scan.nmap_syn_scan('127.0.0.1')
-print(result)
+nmap = nmap3.Nmap()
+result = nmap.nmap_version_detection("192.168.31.135")
+for i in result["192.168.31.135"]["ports"]:
+	print(i["protocol"], i["portid"], i["state"], i["service"]["name"], i["service"]["version"])
