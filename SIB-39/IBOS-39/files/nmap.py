01655693a9de11add32773421d1371edb0d30cdd
ivanlitovka <ewanlitovka@yandex.ru>
2024-05-30 16:58:12 +0300

    33

--- SIB-39/IBOS-39/files/nmap.py
+++ SIB-39/IBOS-39/files/nmap.py
@@ -1,6 +1,6 @@
 import nmap3
 
 nmap = nmap3.Nmap()
-result = nmap.nmap_version_detection("192.168.31.135")
-for i in result["192.168.31.135"]["ports"]:
+result = nmap.nmap_version_detection("127.0.0.1")
+for i in result["127.0.0.1"]["ports"]:
 	print(i["protocol"], i["portid"], i["state"], i["service"]["name"], i["service"]["version"])
