b4da6a3a52f470128ab25bffba825b1d1bb7a993
Ivan Litovka <ivan.litovka@gmail.com>
2025-12-07 22:13:08 +0300

    add add_by_note

--- DE/python/friger.py
+++ DE/python/friger.py
@@ -1,49 +1,30 @@
 import datetime as dt
 from decimal import Decimal
 
+
+DATE_FORMAT = '%Y-%m-%d'
 # Холодильник пуст:
 goods = {}
 
 # Код, который добавляет продукты в словарь goods.
 
 def add(items, title, amount, expiration_date=None):
-    if items[title] not in goods:
-        title = {}
-        print(items[title])
+    if title not in items:
+        items[title] = []
+
+    if expiration_date:
+        expiration_date = dt.datetime.strptime(expiration_date, DATE_FORMAT).date()
+
+    items[title].append({
+        'amount': amount,
+        'expiration_date': expiration_date
+    })
+
+def add_by_note(items, note):
+
 # Добавляем продукт с названием 'Яйца', количество - 10 шт.
 add(goods, 'Яйца', Decimal('10'), '2023-9-30')
-
-# Словарь goods должен стать таким:
-
-# {
-#     'Яйца': [
-#         {'amount': Decimal('10'), 'expiration_date': datetime.date(2023, 9, 30)}
-#     ]
-# }
-
-# Снова добавляем продукт с тем же названием, количество - 3 шт.
 add(goods, 'Яйца', Decimal('3'), '2023-10-15')
-
-# Словарь goods должен стать таким:
-
-# {
-#     'Яйца': [
-#         {'amount': Decimal('10'), 'expiration_date': datetime.date(2023, 9, 30)},
-#         {'amount': Decimal('3'), 'expiration_date': datetime.date(2023, 10, 15)}
-#     ]
-# }
-
-# Добавляем продукт с названием 'Вода', количество - 2.5 кг
 add(goods, 'Вода', Decimal('2.5'))
-
-# Словарь goods должен стать таким:
-
-# {
-#     'Яйца': [
-#         {'amount': Decimal('10'), 'expiration_date': datetime.date(2023, 9, 30)},
-#         {'amount': Decimal('3'), 'expiration_date': datetime.date(2023, 10, 15)}
-#     ],
-#     'Вода': [
-#         {'amount': Decimal('2.5'), 'expiration_date': None}
-#     ]
-# } 
+add_by_note(goods, 'Яйца гусиные 4 2023-07-15')
+print(goods)
